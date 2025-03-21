ea52757cfe83028fa7e1de9b1f4a82f59b614a3a
Jim Johnson <jim@jimjohnson.dev>
2025-03-21 14:45:41 -0400

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import { defineIconGithub, defineIconLinkedin } from '@jjohnson/icon-web-components';
+
+window.onload = () => {
+  console.log('Hello, world!');
+  defineIconGithub();
+  defineIconLinkedin();
+};
