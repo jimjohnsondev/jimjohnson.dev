18f04e511c29d451953abd984dd292ac75a30652
Jim Johnson <jim@jimjohnson.dev>
2025-03-21 15:43:43 -0400

    Remove console log

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import { defineIconGithub, defineIconLinkedin } from '@jjohnson/icon-web-components';
 
 window.onload = () => {
-  console.log('Hello, world!');
   defineIconGithub();
   defineIconLinkedin();
 };
